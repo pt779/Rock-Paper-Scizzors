c6f1ca62e634cd6729db4be9c5dc86d2866e6011
pt779 <pt779@hotmail.co.uk>
2023-09-09 15:51:30 +0100

    playRound funct updated/tested for win scenarios

--- myscripts.js
+++ myscripts.js
@@ -8,17 +8,19 @@ const playerSelection = choices;
 const computerSelection = getComputerChoice ();
 
 function playRound (playerSelection, computerSelection) {
-    if (computerSelection === playerSelection) {
-        return "It's a draw";
-    } else if (computerSelection === "rock" && playerSelection === "paper" 
+    if (computerSelection === "rock" && playerSelection === "paper" 
+    || computerSelection === "paper" && playerSelection === "scizzors"
+    || computerSelection === "scizzors" && playerSelection === "rock") {
+        return "Woohoo, you win!";
+    } else if (computerSelection === "rock" && playerSelection === "scizzors" 
     || computerSelection === "paper" && playerSelection === "rock"
     || computerSelection === "scizzors" && playerSelection === "paper") {
         return "Ahh shucks, the computer wins this time";
     } else {
-        return "Woohoo, you win!";
+        return "It's a draw";
     }
 }
 
 console.log(computerSelection)
-console.log(playRound("paper",computerSelection))
+console.log(playRound("rock",computerSelection))
  
