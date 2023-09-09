9ebb9e037ba550e40a0def1f2cc5761ca1320cf8
pt779 <pt779@hotmail.co.uk>
2023-09-09 15:28:45 +0100

    playRound func added & tested with set user choice

--- myscripts.js
+++ myscripts.js
@@ -4,5 +4,18 @@ function getComputerChoice () {
     return choices[(Math.floor(Math.random() * choices.length))];
 }
 
-console.log(getComputerChoice ())
+const playerSelection = "rock";
+const computerSelection = getComputerChoice ();
+
+function playRound (playerSelection, computerSelection) {
+    if (computerSelection === "Rock") {
+        return "It's a draw";
+    } else if (computerSelection === "Paper") {
+        return "Ahh shucks, the computer wins this time";
+    } else {
+        return "Woohoo, you win!";
+    }
+}
+
+console.log(playRound(playerSelection,computerSelection))
  
