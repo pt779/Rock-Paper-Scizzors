3f17dd9eb33840ef141718b9682bd6d925823094
pt779 <pt779@hotmail.co.uk>
2023-09-11 16:55:32 +0100

    null and undefined alerts created/tested

--- myscripts.js
+++ myscripts.js
@@ -4,20 +4,17 @@ function getComputerChoice () {
     return choices[(Math.floor(Math.random() * choices.length))];
 }
 
-function getPlayerChoice() {
+function getPlayerChoice () {
     let playerChoice = prompt ("rock, paper or scizzors?")
-    playerChoice = playerChoice.toLowerCase()
-    
-    if (playerChoice === "rock" || playerChoice === "paper" || playerChoice === "scizzors") {
-        return playerChoice;
-    } else if (playerChoice === undefined || playerChoice === null)  {
-        alert ("I hope you play again another time")
+    if (playerChoice === "" 
+    || playerChoice === null) {
+    alert ("I hope you play another time");
+    return "Game could not be played";
     } else {
-        alert ("You didn't type in your choice correctly, try reading a dictionary") 
+        return playerChoice.toLowerCase ()
     }
-    
 }
-
+ 
 const playerSelection = getPlayerChoice ();
 const computerSelection = getComputerChoice ();
 
@@ -25,20 +22,19 @@ function playRound (playerSelection, computerSelection) {
     if (computerSelection === "rock" && playerSelection === "paper" 
     || computerSelection === "paper" && playerSelection === "scizzors"
     || computerSelection === "scizzors" && playerSelection === "rock") {
-        return `The computer chose ${computerSelection}
+        return `The computer chose ${computerSelection};
 Woohoo, you win!`;
     } else if (computerSelection === "rock" && playerSelection === "scizzors" 
     || computerSelection === "paper" && playerSelection === "rock"
     || computerSelection === "scizzors" && playerSelection === "paper") {
-        return `The computer chose ${computerSelection}
+        return `The computer chose ${computerSelection};
 Ahh shucks, the computer wins this time`;
     } else if (computerSelection === playerSelection) {
-        return `The computer chose ${computerSelection}
+        return `The computer chose ${computerSelection};
 It's a draw`;
     } else {
-        return "Game could not be played"
+        return "Game could not be played";
     }
 }
 
-console.log(playRound(playerSelection,computerSelection))
- 
+console.log(playRound(playerSelection, computerSelection))
